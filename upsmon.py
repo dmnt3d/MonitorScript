8ffc8db98fb9301937588f5b35d3571c6e3dc09a
dmnt3d <dmented.mind@gmail.com>
2016-08-07 18:17:45 +0800

    UPS Monitor script
    Initial UPSMON

---
+++ upsmon.py
@@ -0,0 +1,21 @@
+import _thread
+import time
+
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+
+
+
+
+try:
+   _thread.start_new_thread( print_time, ("Thread-1", 5, ) )
+   _thread.start_new_thread( print_time, ("Thread-2", 5, ) )
+except:
+   print ("Error: unable to start thread")
+
+while 1:
+   pass
