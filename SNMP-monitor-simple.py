048212d00a646c3237ec397e1bf021a59bf41b7c
dmnt3d <dmented.mind@gmail.com>
2016-10-22 15:48:13 +0800

    Add logging

--- SNMP-monitor-simple.py
+++ SNMP-monitor-simple.py
@@ -2,24 +2,37 @@ import csv
 import subprocess
 import re
 import logging
+import time
+
+from logging.handlers import RotatingFileHandler
 
 #import _thread
 
-log-filename = "SNMP-monitor.log"
+logFilename = "SNMP-monitor.log"
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+logger  = logging.getLogger()
+
+handler = RotatingFileHandler(logFilename, maxBytes=100, backupCount=5)
+logger.addHandler(handler)
+
+logger.debug("Starting Application")
 
 
 def GetPostData(community, target, OID):
+    logger.debug("Getting Post Data via SNMPGet")
     result = subprocess.check_output(["snmpget", "-v", "2c","-c",community,target,OID,"-Ov"])
     result = re.findall("[-+]?\d+[\.]?\d*", result)
     # substring the result
     return result[0]
 
 def PostData (targetInflux, host, target, sensor, value):
+    logger.debug("Posting data")
     postData = subprocess.check_output(["curl", "-i", "-XPOST","http://"+targetInflux+":8086/write?db=home","--data-binary",host + ",host="+ target +",sensor="+sensor+" value=" + value])
     return
 
 def Process(targetInflux,host,target,sensor,community,OID):
+
     value = GetPostData(community,target,OID)
     print ("Adding: " + sensor + " with VALUE: " + value)
     PostData(targetInflux,host,sensor,value)
@@ -35,6 +48,7 @@ def importCSV ():
 
 def main():
     influxDB = "dockerhost.ldc.int"
+    logger.debug("Importing CSV Data")
     csvData = importCSV()
     for item in csvData:
         Process(influxDB,item['host'],item['target'],item['sensor'],item['community'],item['OID'])
