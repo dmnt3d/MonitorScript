35cd8dd8acd25050b77f79b4ad88907680ef1a30
admin <admin@dockerHost.ldc.int>
2016-10-25 18:49:17 +0800

    added new way to post

--- SNMP-monitor-simple.py
+++ SNMP-monitor-simple.py
@@ -3,12 +3,12 @@ import subprocess
 import re
 import logging
 import time
-
+import requests
 from logging.handlers import RotatingFileHandler
 
 #SET FORMATTING for LOG
-logFilename = "SNMP-monitor.log"
-# logFilename = "/home/admin/Scripts/Python/MonitorScript/log/SNMP-monitor.log"
+#logFilename = "SNMP-monitor.log"
+logFilename = "/home/admin/Scripts/Python/MonitorScript/log/SNMP-monitor.log"
 
 formatter = logging.Formatter("%(asctime)s[%(levelname)s]: %(message)s")
 handler = RotatingFileHandler(logFilename, maxBytes=104800, backupCount=5)
@@ -37,23 +37,30 @@ def GetPostData(community, target, OID, table):
         return result[0]
 
 def PostData (targetInflux, table, target, sensor, value):
-    logger.debug("Posting data")
+    logger.debug("Posting data for : [" +target+"]")
     while True:
         try:
             # try posting data
-            postData = subprocess.check_output(["curl", "-i", "-XPOST","http://"+targetInflux+":8086/write?db=home","--data-binary",  table +",host="+ target +",sensor="+sensor+" value=" + value])
-        except subprocess.CalledProcessError as e:
-            logger.debug("FAILED posting with error: "  +  e.output)
-            logger.debug("Retrying after 30 seconds... ")
-            time.sleep(30)
-            continue
-        break
+        logger.debug("PostCMD: curl -i -XPOST http://"+targetInflux+":8086/write?db=home --data-binary '"+ table +",host="+ target +",sensor="+sensor+" value=" + value + "'")
+           # postData = subprocess.check_output(["curl", "-i", "-XPOST","http://"+targetInflux+":8086/write?db=home","--data-binary",  "'" + table +",host="+ target +",sensor="+sensor+" value=" + value +  "'"])
+    data  =  table +",host="+ target +",sensor="+sensor+" value=" + value
+	    response = requests.post ("http:///"+targetInflux+":8086/write?db=home",data)
+	    logger.debug("Done Posting")
+	    if (response  == "<Response [204]>"):
+	         break
+            else:
+#        except data as e:
+                 logger.debug("FAILED posting. Retrying after 30 seconds... ")
+                 time.sleep(30)
+                 continue
+#        break
 
+    logger.debug ("Continue execution")
     return
 
 def Process(targetInflux,table,target,sensor,community,OID):
     value = GetPostData(community,target,OID,table)
-    logger.debug("Adding: " + sensor + " with VALUE: " + value)
+    logger.debug("Adding: [" + sensor + "] with VALUE: [" + value + "]")
     #print ("Adding: " + sensor + " with VALUE: " + value)
     PostData(targetInflux,table,target,sensor,value)
     return
@@ -64,8 +71,8 @@ def Process(targetInflux,table,target,sensor,community,OID):
 
 def importCSV ():
     logger.debug("Importing the CSV data")
-    input_file = csv.DictReader(open("D:\Python\MonitorScript\data.csv"))
-#   input_file = csv.DictReader(open("/home/admin/Scripts/Python/MonitorScript/data.csv"))
+#    input_file = csv.DictReader(open("D:\Python\MonitorScript\data.csv"))
+    input_file = csv.DictReader(open("/home/admin/Scripts/Python/MonitorScript/data.csv"))
 
     return input_file
 
@@ -76,11 +83,15 @@ def mainLoop (csvData, influxDB):
 
 def main():
     influxDB = "dockerhost.ldc.int"
-    csvData = importCSV()
+    #csvData = importCSV()
     while True:
+	csvData = importCSV()
         mainLoop(csvData, influxDB)
+	logger.debug ("Preparing for next execution cycle after 30 secs")
         time.sleep(30)
-
+	logger.debug ("Executing next Cycle")
+	
+	
 
 if __name__ == "__main__":
     logger.debug("Starting Application")
